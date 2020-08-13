b73ad6057f651349f8d8975047f097f6bdaea72a
Danny Li <dongledan@yahoo.com>
2020-08-12 23:02:05 -0400

    store update

--- client/store/index.js
+++ client/store/index.js
@@ -2,9 +2,15 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import user from './user'
+import {userReducer} from './user'
+import {productsReducer} from './productList'
+import {cartReducer} from './cart'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({
+  user: userReducer,
+  products: productsReducer,
+  cart: cartReducer
+})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
